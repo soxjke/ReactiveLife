10ff8e9c5cea0b241a361436fc54b218a9ce245d
Petro Korienev <soxjke@gmail.com>
2017-03-03 23:05:24 +0200

    Reactive search - first version

--- CompositeTableView/ViewController.swift
+++ CompositeTableView/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        @IBOutlet weak var searchField: UITextField!
         // Do any additional setup after loading the view, typically from a nib.
     }
 
--- ReactiveSearch/ViewController.swift
+++ ReactiveSearch/ViewController.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
+import CoreLocation
+import ReactiveCocoa
+import ReactiveSwift
 
 class ViewController: UIViewController {
 
+    private let geocoder: CLGeocoder = CLGeocoder()
+    fileprivate var placemarks: [CLPlacemark]? = nil
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupBindings()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func setupBindings() {
+        searchTextField.reactive.continuousTextValues.skipNil().observeValues { [weak self] (string) in
+            self?.geocoder.geocodeAddressString(string) { (placemarks, error) in
+                DispatchQueue.main.async { [weak self] in
+                    self?.placemarks = placemarks
+                    self?.tableView.reloadData()
+                }
+            }
+        }
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return placemarks?.count ?? 0
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "MyCell")
+        cell = cell ?? UITableViewCell(style: .default, reuseIdentifier: "MyCell")
+        cell?.imageView?.image = UIImage(named: "icLocation")
+        cell?.textLabel?.text = placemarks?[indexPath.row].formattedAddress
+        return cell!
+    }
+}
+
+extension ViewController: UITableViewDelegate {}
+
+extension CLPlacemark {
+    var formattedAddress : String {
+        get {
+            var result = ""
+            cast(addressDictionary?["FormattedAddressLines"]) { (addressLines: NSArray) in
+                result = addressLines.componentsJoined(by: ", ")
+            }
+            return result
+        }
+    }
+}
